1c59b14cf3a58d3c08d87c79576f36cf54361be7
Jax <qianguangxin12345@gmail.com>
2021-02-27 22:54:53 -0500

    Enter

--- action.js
+++ action.js
@@ -29,34 +29,156 @@ function calculate(){
     var r = -1;
     var v = -1;
     var i = -1;
+
+    // Initialized of variables as string
+    var pUnit = document.getElementById("Power Unit").value;
+    var rUnit = document.getElementById("Resistance Unit").value;
+    var vUnit = document.getElementById("Voltage Unit").value;
+    var iUnit = document.getElementById("Current Unit").value;
+
     // Resign the value according to the user's input
     p = getPower();
     r = getResistance();
     v = getVoltage();
     i = getCurrent();
+
     if(p != 0 && r != 0){       // Calculate v and i
-        document.getElementById('voltage').value = Math.sqrt(p * r);
-        document.getElementById('current').value = Math.sqrt(p / r);
+        // Measure p in terms of Watts
+        if(pUnit == "Kilowatts"){
+            p = p * 1000;
+        }
+        else if(pUnit == "milliwatts"){
+            p = p / 1000;
+        }
+
+        // Measure r in terms of ohms
+        if(rUnit == "Kiloohms"){
+            r = r * 1000;
+        }
+        else if(rUnit == "Milliohms"){
+            r = r / 1000;
+        }
+        
+        v = Math.sqrt(p * r);
+        i = Math.sqrt(p / r);
+
+        document.getElementById('voltage').value = v;
+        document.getElementById('current').value = i;
     }
     else if(p != 0 && v != 0){  // Calculate r and i
-        document.getElementById('resistance').value = v * v / p;
-        document.getElementById('current').value = p / v;
+        // Measure p in terms of Watts
+        if(pUnit == "Kilowatts"){
+            p = p * 1000;
+        }
+        else if(pUnit == "milliwatts"){
+            p = p / 1000;
+        }
+
+        // Measure v in terms of volt
+        if(vUnit == "Kilovolts"){
+            v = v * 1000;
+        }
+        else if(vUnit == "Millivolts"){
+            v = v / 1000;
+        }
+        
+        r = v * v / p;
+        i = p / v;
+        
+        document.getElementById('resistance').value = r;
+        document.getElementById('current').value = i;
     }
     else if(p != 0 && i != 0){  // Calculate r and v
-        document.getElementById('resistance').value = p / (i * i);
-        document.getElementById('voltage').value = p / i;
+        // Measure p in terms of Watts
+        if(pUnit == "Kilowatts"){
+            p = p * 1000;
+        }
+        else if(pUnit == "milliwatts"){
+            p = p / 1000;
+        }
+
+        // Measure i in terms of ampere
+        if(iUnit == "Kiloampere"){
+            i = i * 1000;
+        }
+        else if(iUnit == "Milliampere"){
+            i = i / 1000;
+        }
+
+        r = p / (i * i);
+        v = p / i;
+
+        document.getElementById('resistance').value = r;
+        document.getElementById('voltage').value = v;
     }
     else if(r != 0 && v != 0){  // Calculate p and i
-        document.getElementById('power').value = v * v / r;
-        document.getElementById('current').value = v / r;
+        // Measure r in terms of ohms
+        if(rUnit == "Kiloohms"){
+            r = r * 1000;
+        }
+        else if(rUnit == "Milliohms"){
+            r = r / 1000;
+        }
+
+        // Measure v in terms of volt
+        if(vUnit == "Kilovolts"){
+            v = v * 1000;
+        }
+        else if(vUnit == "Millivolts"){
+            v = v / 1000;
+        }
+        
+        p = v * v / r;
+        i = v / r;
+        
+        document.getElementById('power').value = p;
+        document.getElementById('current').value = i;
     }
     else if(r != 0 && i != 0){  // Calculate p and v
-        document.getElementById('power').value = i * i * r;
-        document.getElementById('voltage').value = i * r;
+        // Measure r in terms of ohms
+        if(rUnit == "Kiloohms"){
+            r = r * 1000;
+        }
+        else if(rUnit == "Milliohms"){
+            r = r / 1000;
+        }
+
+        // Measure i in terms of ampere
+        if(iUnit == "Kiloampere"){
+            i = i * 1000;
+        }
+        else if(iUnit == "Milliampere"){
+            i = i / 1000;
+        }
+
+        p = i * i * r;
+        v = i * r;
+
+        document.getElementById('power').value = p;
+        document.getElementById('voltage').value = v;
     }
     else if(v != 0 && i != 0){  // Calculate p and r
-        document.getElementById('power').value = v * i;
-        document.getElementById('resistance').value = v / i;
+        // Measure v in terms of volt
+        if(vUnit == "Kilovolts"){
+            v = v * 1000;
+        }
+        else if(vUnit == "Millivolts"){
+            v = v / 1000;
+        }
+
+        // Measure i in terms of ampere
+        if(iUnit == "Kiloampere"){
+            i = i * 1000;
+        }
+        else if(iUnit == "Milliampere"){
+            i = i / 1000;
+        }
+
+        p = v * i;
+        r = v / i;
+
+        document.getElementById('power').value = p;
+        document.getElementById('resistance').value = r;
     }
 }
 
